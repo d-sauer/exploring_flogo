de5c33d0b3ec7fbaba03a4485e75181052f81de1
d-sauer <davor.sauer@gmail.com>
2021-05-12 21:39:07 +0200

    Clean descriptor for unused properties.

--- trigger/log-level-trigger/metadata.go
+++ trigger/log-level-trigger/metadata.go
@@ -1,49 +1,45 @@
 package log_level
 
-import (
-	"github.com/project-flogo/core/data/coerce"
-)
-
 type Settings struct {
-	ASetting int `md:"aSetting,required"`
+	//ASetting int `md:"aSetting,required"`
 }
 
 type HandlerSettings struct {
-	ASetting string `md:"aSetting,required"`
+	//ASetting string `md:"aSetting,required"`
 }
 
 type Output struct {
-	AnOutput string `md:"anOutput"`
+	//AnOutput string `md:"anOutput"`
 }
 
 func (o *Output) FromMap(values map[string]interface{}) error {
 
-	var err error
-	o.AnOutput, err = coerce.ToString(values["anOutput"])
-	if err != nil {
-		return err
-	}
+	//var err error
+	//o.AnOutput, err = coerce.ToString(values["anOutput"])
+	//if err != nil {
+	//	return err
+	//}
 
 	return nil
 }
 
 func (o *Output) ToMap() map[string]interface{} {
 	return map[string]interface{}{
-		"anOutput": o.AnOutput,
+		//"anOutput": o.AnOutput,
 	}
 }
 
 type Reply struct {
-	AReply interface{} `md:"aReply"`
+	//AReply interface{} `md:"aReply"`
 }
 
 func (r *Reply) FromMap(values map[string]interface{}) error {
-	r.AReply = values["aReply"]
+	//r.AReply = values["aReply"]
 	return nil
 }
 
 func (r *Reply) ToMap() map[string]interface{} {
 	return map[string]interface{}{
-		"aReply": r.AReply,
+		//"aReply": r.AReply,
 	}
 }
